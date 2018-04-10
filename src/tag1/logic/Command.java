28bc8cd0cd4bfe64a9caf1f3cfb88d3f61f69a47
nikor <nikor@10.50.137.69>
2018-04-10 11:44:40 +0200

    Creating classes

--- src/tag1/logic/Command.java
+++ src/tag1/logic/Command.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Command {
-    
+
     Scanner scan = new Scanner(System.in);
-    
+
     private String north = "north";
     private String south = "south";
     private String east = "east";
@@ -15,7 +15,7 @@ public class Command {
     private String help = "help";
     private ArrayList allowedCommands = new ArrayList();
     private boolean quitGame = false;
-    
+
     public ArrayList getAllowedCommands() {
         return allowedCommands;
     }
@@ -27,8 +27,7 @@ public class Command {
     public void setQuitGameBoolean(boolean quitGame) {
         this.quitGame = quitGame;
     }
-    
-    
+
     public void addCommands(String commandName) {
         allowedCommands.add(north);
         allowedCommands.add(south);
@@ -37,13 +36,37 @@ public class Command {
         allowedCommands.add(quit);
         allowedCommands.add(help);
     }
-    
-    public void quitGame(String quit) {
-        if(scan.next() == quit) {
+        public void quitGame(String input) {
+        if (input == quit) {
             setQuitGameBoolean(true);
             System.out.println("Quitting game in 10 seconds..");
             System.exit(10);
         }
     }
+
+    public String gameInformation() {
+            return "The game is a test";
+    }
     
+    public void gameCommands() {
+        for (Object allowedCommand : allowedCommands) {
+            System.out.println(allowedCommand);
+
+        }
+    }
+    
+        public void help(String input) {
+        if (input == help) {
+            System.out.println("Select one of the options below: ");
+        }
+        System.out.println("1: for game information ");
+        System.out.println("2: for game commands ");
+        switch (scan.next()) {
+            case "1":
+                gameInformation();
+            case "2":
+                gameCommands();
+        }
+    }
+
 }
